460384fc6dca0a4d595fe9b3090cf49231a38b54
Khabir Ahmed <khabirahmed780@gmail.com>
2024-08-08 16:02:27 +0530

    ..

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ const colors = require("colors");
 const moragan = require("morgan");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
-const path = require('path')
-
-
+const path = require("path");
 
 //dotenv conig
 dotenv.config();
@@ -26,12 +24,11 @@ app.use("/api/v1/admin", require("./routes/adminRoutes"));
 app.use("/api/v1/doctor", require("./routes/doctorRoutes"));
 
 // static files
-app.use(express.static(path.join(__dirname, './client/build')))
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
-
 
 //port
 const port = process.env.PORT || 8080;
